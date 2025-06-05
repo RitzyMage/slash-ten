7fa3ead76d82f3efcd1a39f0ca10d8aff8b5cdc2
Avery Green <averyg@utahcounty.gov>
2025-06-05 08:08:04 -0600

    fetch user task

--- src/lib/server/tasks/update-task.ts
+++ src/lib/server/tasks/update-task.ts
@@ -1,7 +1,12 @@
+import UpdateUsersTask from "./update-users-task";
 import TaskSequence from "./task-sequence";
 import UpdateBooksTask from "./update-books-task";
 import UpdateClientsTask from "./update-clients-task";
 
 export default async function GetUpdateTask() {
-  return new TaskSequence([new UpdateClientsTask(), new UpdateBooksTask()]);
+  return new TaskSequence([
+    new UpdateClientsTask(),
+    new UpdateBooksTask(),
+    new UpdateUsersTask(),
+  ]);
 }
---
+++ src/lib/server/tasks/update-users-task.ts
@@ -0,0 +1,30 @@
+import { Status } from "$lib/data-types/task-info";
+import GetBookReviewersTask from "./get-book-reviewers-task";
+import GetUserReviewsTask from "./get-user-reviews-task";
+import Task from "./task";
+import TaskSequenceWithInitialize from "./task-sequence-with-initialize";
+
+const CHUNKS = 10;
+const TIME = 200;
+
+export default class UpdateUsersTask extends TaskSequenceWithInitialize {
+  protected async GetSequence(): Promise<Task[]> {
+    // IMPLEMENT III: get users with a stale cache
+    for (let i = 0; i < CHUNKS; ++i) {
+      await new Promise((res) => setTimeout(res, TIME / CHUNKS));
+      let timeLeft = ((1 - i / CHUNKS) * TIME) / 1000;
+      this.updateStatus({
+        status: Status.IN_PROGRESS,
+        message: `Fetching book list (${timeLeft.toFixed(2)}s left)`,
+        completion: i / CHUNKS,
+      });
+    }
+
+    let users = [30, 100, 2000];
+    return users.map((userId) => new GetUserReviewsTask(userId));
+  }
+
+  protected get prefix(): string {
+    return "getting all users";
+  }
+}
