ac642bc9197cc2bce2e2c7683c34c340c020744e
Avery Green <averyg@utahcounty.gov>
2026-02-13 09:09:38 -0700

    get media list

--- src/lib/server/tasks/update-user-book-similarity-task.ts
+++ src/lib/server/tasks/update-user-book-similarity-task.ts
@@ -1,25 +1,63 @@
-import { Status } from "$lib/task-info";
-import Task from "./task";
+import { Status } from '$lib/task-info';
+import { eq, sql } from 'drizzle-orm';
+import { db } from '../db';
+import { clients, userClientSimilarity, users } from '../db/schema';
+import Task from './task';
+import type { Review } from '../db/types';
 
-const CHUNKS = 20;
-const TIME = 2000;
+const MIN_COMMON_REVIEWS = 20;
+
+async function getValidMedia(clientId: number, minCommonReviews: number): Promise<Review[]> {
+	// let similarUsers = await db
+	// 	.select()
+	// 	.from(userClientSimilarity)
+	// 	.where(eq(userClientSimilarity.clientId, clientId));
+	return await db.execute(sql`
+      WITH ValidMedia AS (
+          SELECT r."mediaId"
+          FROM "Review" r
+          JOIN "UserClientSimilarity" ON r."userId" = "UserClientSimilarity"."userId"
+          WHERE "UserClientSimilarity"."clientId" = ${clientId}
+          GROUP BY "mediaId"
+          HAVING COUNT(DISTINCT r."userId") >= ${minCommonReviews}
+      )
+      SELECT r.*
+      FROM "Review" r
+      JOIN ValidMedia ON r."mediaId" = ValidMedia."mediaId";
+    `);
+}
 
 export default class UpdateUserBookSimilarityTask extends Task {
-  protected async _Run(): Promise<void> {
-    // IMPLEMENT IV: fetch user - book combos that need similarity updated, update similarity
-    for (let i = 0; i < CHUNKS; ++i) {
-      await new Promise((res) => setTimeout(res, TIME / CHUNKS));
-      let timeLeft = ((1 - i / CHUNKS) * TIME) / 1000;
-      this.updateStatus({
-        status: Status.IN_PROGRESS,
-        message: `Updating book-user similarity (${timeLeft.toFixed(2)}s left)`,
-        completion: i / CHUNKS,
-      });
-    }
-    this.updateStatus({
-      status: Status.SUCCESSFUL,
-      message: `Finished updating book-user similarity`,
-      completion: 1,
-    });
-  }
+	protected async _Run(): Promise<void> {
+		let allClientsWithUsers = await db
+			.select()
+			.from(clients)
+			.innerJoin(users, eq(users.id, clients.userId));
+
+		this.updateStatus({
+			status: Status.IN_PROGRESS,
+			message: `finished getting reviews`,
+			completion: 0.1,
+		});
+		await new Promise((res) => setTimeout(res, 2000));
+
+		for (let _i in allClientsWithUsers) {
+			let i = parseInt(_i);
+			let { Client, User } = allClientsWithUsers[i];
+			let validMedia = await getValidMedia(Client.id, MIN_COMMON_REVIEWS);
+			console.log('for client', Client.name, 'valid media is', validMedia.length);
+
+			this.updateStatus({
+				status: Status.IN_PROGRESS,
+				message: `finished client ${Client.name} (${i + 1} /${allClientsWithUsers.length})`,
+				completion: 0.1 + i / allClientsWithUsers.length / 0.9,
+			});
+		}
+
+		this.updateStatus({
+			status: Status.SUCCESSFUL,
+			message: `Finished updating book-user similarity`,
+			completion: 1,
+		});
+	}
 }
