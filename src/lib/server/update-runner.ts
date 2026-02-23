393fa6ad2971fcb5ec6910a5a1e6ae36f3507438
Avery Green <averyg@utahcounty.gov>
2026-02-23 10:41:56 -0700

    add update history entry on finish

--- src/lib/server/update-runner.ts
+++ src/lib/server/update-runner.ts
@@ -1,34 +1,43 @@
-import Task from "./tasks/task";
-import { db } from "./db";
-import { updateHistory } from "./db/schema";
+import type Task from './tasks/task';
+import { db } from './db';
+import { updateHistory } from './db/schema';
+import type TaskObserver from './tasks/task-observer';
+import { isComplete, Status, type TaskDetails } from '$lib/task-info';
 
-class UpdateRunner {
-  public RunTask(task: Task) {
-    if (this.task) {
-      throw new Error("Cannot start a new task while one is running!");
-    }
-    this.task = task;
-    task.Run(() => this.RemoveTask());
-  }
+class UpdateRunner implements TaskObserver {
+	async notify(info: TaskDetails): Promise<void> {
+		if (isComplete(info)) {
+			await db.insert(updateHistory).values({
+				ran: new Date(),
+				success: info.status === Status.SUCCESSFUL,
+				updateData: JSON.stringify(info),
+			});
+		}
+	}
 
-  private async RemoveTask() {
-    if (!this.task) {
-      throw new Error(`Not currently running a task`);
-    }
-    await db.insert(updateHistory).values({
-      ran: new Date(),
-      updateData: JSON.stringify(this.task.currentInfo),
-    });
-    this.task = undefined;
-  }
+	public RunTask(task: Task) {
+		if (this.task) {
+			throw new Error('Cannot start a new task while one is running!');
+		}
+		this.task = task;
+		task.addObserver(this);
+		task.Run(() => this.RemoveTask());
+	}
 
-  private set task(task: Task | undefined) {
-    globalThis.__task = task;
-  }
+	private async RemoveTask() {
+		if (!this.task) {
+			throw new Error(`Not currently running a task`);
+		}
+		this.task = undefined;
+	}
 
-  public get task() {
-    return globalThis.__task;
-  }
+	private set task(task: Task | undefined) {
+		globalThis.__task = task;
+	}
+
+	public get task() {
+		return globalThis.__task;
+	}
 }
 
 export default UpdateRunner;
