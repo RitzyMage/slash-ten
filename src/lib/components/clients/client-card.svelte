5974401d82e48bafdc4f90cbd08eec8b746153fe
Avery Green <averyg@utahcounty.gov>
2026-02-26 09:37:58 -0700

    styled client card

--- src/lib/components/clients/client-card.svelte
+++ src/lib/components/clients/client-card.svelte
@@ -4,7 +4,11 @@
   export let client: Client;
 </script>
 
-<div class="clientCard">{JSON.stringify(client)}</div>
+<div class="clientCard">
+  <h2 class="clientCard-name">
+    {client.name} <span class="clientCard-id">(id: {client.id})</span>
+  </h2>
+</div>
 
 <style>
   .clientCard {
@@ -19,5 +23,9 @@
   .clientCard:hover {
     box-shadow: 2px 2px 4px rgba(0, 0, 0, 80%);
     background-color: var(--bg-light);
+  }
+
+  .clientCard-id {
+    color: var(--text-muted);
   }
 </style>
