df1656d4f1594313c85c2f0071592977b5f51263
Avery Green <averyg@utahcounty.gov>
2026-02-26 09:34:12 -0700

    basic client card

---
+++ src/lib/components/clients/client-card.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import type { Client } from "$lib/server/db/types";
+
+  export let client: Client;
+</script>
+
+<div class="clientCard">{JSON.stringify(client)}</div>
+
+<style>
+  .clientCard {
+    background-color: var(--bg);
+    box-shadow: 1px 1px 2px rgba(0, 0, 0, 50%);
+    border-radius: var(--rounded);
+    padding: var(--2);
+
+    cursor: pointer;
+  }
+
+  .clientCard:hover {
+    box-shadow: 2px 2px 4px rgba(0, 0, 0, 80%);
+    background-color: var(--bg-light);
+  }
+</style>
--- src/lib/components/clients/clients-page.svelte
+++ src/lib/components/clients/clients-page.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
   import type { Client } from "$lib/server/db/types";
+  import ClientCard from "./client-card.svelte";
 
   export let clients: Client[];
 </script>
 
 <h1>Clients</h1>
 {#each clients as client}
-  <div>{JSON.stringify(client)}</div>
+  <ClientCard {client} />
 {/each}
