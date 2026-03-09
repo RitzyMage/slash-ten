d2448d9e1d18d3239e794e638ec168e95942dd5f
Avery Green <averyg@utahcounty.gov>
2026-03-09 10:55:22 -0600

    remove default client

--- src/lib/tmp/default-client.ts
+++
@@ -1,3 +0,0 @@
-const DEFAULT_CLIENT = 1;
-
-export default DEFAULT_CLIENT;
--- src/routes/recommendations/[id]/+server.ts
+++ src/routes/recommendations/[id]/+server.ts
@@ -1,13 +1,12 @@
 import { db } from "$lib/server/db";
 import { media, userPredictedScores } from "$lib/server/db/schema";
 import { desc, eq } from "drizzle-orm";
-import DEFAULT_CLIENT from "$lib/tmp/default-client";
 
-export async function GET() {
+export async function GET({params}) {
 return Response.json( await db
       .select()
       .from(userPredictedScores)
-      .where(eq(userPredictedScores.clientId, DEFAULT_CLIENT))
+      .where(eq(userPredictedScores.clientId, parseInt(params.id)))
       .innerJoin(media, eq(media.id, userPredictedScores.mediaId))
       .orderBy(desc(userPredictedScores.score))
       .limit(50),
